7ed1317dd9cf93b3f7eac72826b60256b1200f51
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-10-29 01:38:46 -0700

    Update unnessesary Purchase service unnessesary sqs import, remove rabbitTest, remove yml rabbit section

--- PurchaseService/src/main/java/org/java/purchaseservice/config/MessageSnsConfig.java
+++ PurchaseService/src/main/java/org/java/purchaseservice/config/MessageSnsConfig.java
@@ -1,11 +1,9 @@
 package org.java.purchaseservice.config;
 
 import io.awspring.cloud.sns.core.SnsTemplate;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
 public class MessageSnsConfig {
@@ -16,20 +14,8 @@ public class MessageSnsConfig {
   }
 
   @Bean
-  public SqsAsyncClient sqsAsyncClient() {
-    return SqsAsyncClient.create();
-  }
-
-  @Bean
   public SnsTemplate snsTemplate(SnsClient snsClient) {
     return new SnsTemplate(snsClient);
-  }
-
-  @Bean
-  public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
-    return SqsTemplate.builder()
-        .sqsAsyncClient(sqsAsyncClient)
-        .build();
   }
 
 }
